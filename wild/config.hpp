5fd4e207e02206707a1fc775cab545c49db784b7
kloffy <kloffy@gmail.com>
2013-03-16 17:56:30 +0100

    Rudimentary platform config.

--- wild/config.hpp
+++ wild/config.hpp
@@ -1,7 +1,17 @@
 #ifndef WILD_CONFIG_HPP
 #define WILD_CONFIG_HPP
 
-#if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
+#if defined(linux) || defined(__linux) || defined(__linux__) || defined(__GNU__) || defined(__GLIBC__)
+
+#define WILD_PLATFORM_LINUX
+
+#elif defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__)
+
+#define WILD_PLATFORM_MACOS
+
+#elif defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
+
+#define WILD_PLATFORM_WIN32
 
 // Availability depends on platfrom or compiler?
 #define HAVE_BITSCANFORWARD
